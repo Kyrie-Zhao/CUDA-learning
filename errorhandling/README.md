ee7a68ce96ad195a9438ea297cc645b6e2a6679e
Kyrie-Zhao <zz208@duke.edu>
2022-08-26 22:49:59 +0800

    sumarray

---
+++ errorhandling/README.md
@@ -0,0 +1,3 @@
+## Sum Array Error Handling
+
+#### 頭文件裏的內容沒看懂 :(
---
+++ errorhandling/errorhandling.cu
@@ -0,0 +1,100 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include "cuda_common.cuh"
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+__global__ void sumarray(int* a, int* b, int* c, int size){
+    int gid = blockIdx.x * blockDim.x + threadIdx.x;
+    if (gid < size) {
+      c[gid] = a[gid] + b[gid];
+    }   
+}
+
+void sum_array_cpu(int* a, int* b, int* c, int size)
+{
+    for ( int i = 0;i < size;i++ ) {
+        c[i] = a[i] + b[i];      
+    }
+}
+
+void compare_arrays(int* a, int* b, int size)
+{
+    for ( int i = 0;i < size;i++ ) {
+      if (a[i] != b[i]) { 
+          printf("Arrays are different!");
+          return;
+      }
+    }
+    printf("Arrays are same\n");
+}
+
+int main(void){
+    int size = 10000;
+    int block_size = 128;
+    int nBytes = size * sizeof(int);
+
+    int* h_a, *h_b, *gpu_results, *h_c;
+
+    h_a = (int*)malloc(nBytes);
+    h_b = (int*)malloc(nBytes);
+    h_c = (int*)malloc(nBytes);
+    gpu_results = (int*)malloc(nBytes);
+    cudaError error;
+
+
+    time_t t;
+    srand((unsigned)time(&t));
+    for (int i = 0;i < size;i++) {
+        h_a[i] = (int)(rand() & 0xff);
+        h_b[i] = (int)(rand() & 0xff);
+    }
+    
+    sum_array_cpu(h_a, h_b, h_c, size);
+    //printf("%d",&h_a[1]);
+    memset(gpu_results, 0, nBytes);
+
+
+    int* d_a, *d_b, *d_c;
+    error = cudaMalloc((int**)&d_a, nBytes);
+    if (error != cudaSuccess) {
+        fprintf(stderr, "Error : %s \n", cudaGetErrorString(error));
+    }
+    //cudaMalloc((int**)&d_a, nBytes);
+    cudaMalloc((int**)&d_b, nBytes);
+    cudaMalloc((int**)&d_c, nBytes);
+
+    cudaMemcpy(d_a, h_a, nBytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, h_b, nBytes, cudaMemcpyHostToDevice);
+
+
+    // launching the grid
+    dim3 block(block_size);
+    dim3 grid((size / block.x) + 1);
+
+    sumarray<<<grid, block>>> (d_a, d_b, d_c, size);
+    cudaDeviceSynchronize();
+
+
+    cudaMemcpy(gpu_results, d_c, nBytes, cudaMemcpyDeviceToHost);
+
+
+    // array comparison
+    compare_arrays(h_c, gpu_results, size);
+
+
+
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
+    
+
+    free(gpu_results);
+    cudaDeviceReset();
+
+
+    return 0;
+
+}
